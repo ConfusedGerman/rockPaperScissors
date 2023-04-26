d10eb94536f62ab3a1eb7b880058e7aafaf38d7d
ConfusedGerman <okehermanns11@gmail.com>
2023-04-26 13:31:10 +0200

    Implement game function, add comments, fix bugs
    The game function let the user play against the computer until
    someone got five points and wins the round. Additionally I fixed
    some small bugs, forgot the parenteses after the lower case function.
    Also added some comments.

--- javascript.js
+++ javascript.js
@@ -1,34 +1,76 @@
+// Determine computers choice
 function getComputerChoice () {
     let choice = "";
+    // Random number between zero and one
     let random = Math.random();
-    console.log(random);
     if (random <= 1/3) {
-        return choice = "Rock";
+        return choice = "rock";
     }   else if (random <= 2/3) {
-        return choice = "Paper";
+        return choice = "paper";
     }   else {
-        return choice = "Scissors";
+        return choice = "scissors";
     }
 }
 
-let computerChoice = getComputerChoice();
-let playerChoice = getComputerChoice().toLowerCase;
+
 function playRound (computer, player) {
-    if (computerChoice === playerChoice) {
-        return console.log("Tie!")
+    // False input
+    if (player != "rock" && player != "paper" && player != "scissors") {
+        console.log("Only rock, paper and scissors are allowed!")
+        return "Noone";
     }
-    else if (computer == "Rock" && player == "scissors") {
-        return "Computer won!";
+    // Tie
+    if (computer === player) {
+        return "Noone";
+    }
+    else if (computer === "rock" && player === "scissors") {
+        return "Computer";
     }
 
-    else if (computer == "Paper" && player == "rock") {
-        return "Computer won!";
+    else if (computer === "paper" && player === "rock") {
+        return "Computer";
     }
 
-    else if (computer == "Scissors" && player == "paper") {
-        return "Computer won!";
+    else if (computer === "scissors" && player === "paper") {
+        return "Computer";
     }
     else {
-        return "Player won!";
+        return "Player";
     }
 }
+
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (playerScore < 5 && computerScore < 5) {
+        let computerChoice = getComputerChoice();
+        // Ask user for his choice
+        let getPlayerChoice = prompt("Rock, Paper or Scissors? ");
+        let playerChoice = getPlayerChoice.toLowerCase();
+
+        // Play a single round
+        let winner = playRound(computerChoice, playerChoice);
+        //Print winner
+        console.log("Your choice is " + playerChoice + ", the computer chose " + computerChoice + ". " + winner + " won.");
+        // Update score
+        if (winner === "Player") {
+            playerScore ++;
+        }
+        else if (winner === "Computer") {
+            computerScore ++;
+        }
+    }
+
+    // End of game with five pointsS
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore > computerScore) {
+            console.log("You won!");
+        }
+        else {
+            console.log("Computer won!");
+        }
+    }
+}
+// Call function to start game
+game();
