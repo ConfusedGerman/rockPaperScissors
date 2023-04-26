ec1aaa9011836caaf99d6f1e4817f0423611ab99
ConfusedGerman <okehermanns11@gmail.com>
2023-04-26 12:36:10 +0200

    Add function to determine the winner of a round
    the functions has two parameters as input for the two different
    choices and depending on the parameter values it returns the winner

--- javascript.js
+++ javascript.js
@@ -1,11 +1,34 @@
 function getComputerChoice () {
     let choice = "";
     let random = Math.random();
+    console.log(random);
     if (random <= 1/3) {
         return choice = "Rock";
     }   else if (random <= 2/3) {
         return choice = "Paper";
     }   else {
         return choice = "Scissors";
+    }
+}
+
+let computerChoice = getComputerChoice();
+let playerChoice = getComputerChoice();
+function playRound (computer, player) {
+    if (computerChoice === playerChoice) {
+        return console.log("Tie!")
+    }
+    else if (computer == "Rock" && player == "Scissors") {
+        return "Computer won!";
+    }
+
+    else if (computer == "Paper" && player == "Rock") {
+        return "Computer won!";
+    }
+
+    else if (computer == "Scissors" && player == "Paper") {
+        return "Computer won!";
+    }
+    else {
+        return "Player won!";
     }
 }
