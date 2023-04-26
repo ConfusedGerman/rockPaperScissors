150a35d736a9d516f59c964eeed072579d7e7c4a
ConfusedGerman <okehermanns11@gmail.com>
2023-04-26 12:41:42 +0200

    Make player choice case-insensitive

--- javascript.js
+++ javascript.js
@@ -12,20 +12,20 @@ function getComputerChoice () {
 }
 
 let computerChoice = getComputerChoice();
-let playerChoice = getComputerChoice();
+let playerChoice = getComputerChoice().toLowerCase;
 function playRound (computer, player) {
     if (computerChoice === playerChoice) {
         return console.log("Tie!")
     }
-    else if (computer == "Rock" && player == "Scissors") {
+    else if (computer == "Rock" && player == "scissors") {
         return "Computer won!";
     }
 
-    else if (computer == "Paper" && player == "Rock") {
+    else if (computer == "Paper" && player == "rock") {
         return "Computer won!";
     }
 
-    else if (computer == "Scissors" && player == "Paper") {
+    else if (computer == "Scissors" && player == "paper") {
         return "Computer won!";
     }
     else {
